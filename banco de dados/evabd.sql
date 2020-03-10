9ec2dcf0771ddfedc2e5e64eb1de28d98f3da525
marcelowhitehead <marcelo.cacace@bandtec.com.br>
2020-03-10 16:40:10 -0300

    bd atualizado

--- banco de dados/evabd.sql
+++ banco de dados/evabd.sql
@@ -3,7 +3,7 @@ create database eva;
 use eva;
 
 
-create table supermercado(
+create table empresa(
 cnpj varchar(14) primary key,
 empresa_nome varchar(40) not null,
 email varchar(50),
@@ -19,7 +19,7 @@ cpf varchar(11) not null,
 fkcnpj varchar(14),
 
 foreign key (fkcnpj)
-references supermercado(cnpj)
+references empresa(cnpj)
 );
 
 create table mensal(
@@ -31,7 +31,7 @@ economia float,
 fkDados int,
 
 foreign key (fkcnpj)
-references supermercado(cnpj),
+references empresa(cnpj),
 foreign key (fkDados)
 references dados(idDados)
 );
@@ -48,7 +48,7 @@ fkcnpj varchar(14),
 foreign key (fkidsensor)
 references sensores(idsensor),
 foreign key (fkcnpj)
-references supermercado(cnpj)
+references empresa(cnpj)
 );
 
 create table sensores(
@@ -57,29 +57,29 @@ area_sensor varchar(10) not null,
 fkcnpj varchar(14),
 
 foreign key(fkcnpj)
-references supermercado(cnpj)
+references empresa(cnpj)
 );
 
-select * from supermercado;
+select * from empresa;
 
-insert into supermercado values('92864872000116', 'Carrefour', 'carrefour@administraçao.com.br', 'carrefouradm', '(11)5678-9876');
-insert into supermercado values('96239176000170', 'Extra', 'extra@administraçao.com.br', 'extrasuporte', '(11)7654-9812');
-insert into supermercado values('09592804000100', 'Pão de Açucar', 'paodeaçucar@geral.com.br', 'paoacucar123', '(21)6578-3489');
-select * from supermercado;
-delete from supermercado where cnpj = '96239176000170';
+insert into empresa values('92864872000116', 'Carrefour', 'carrefour@administraçao.com.br', 'carrefouradm', '(11)5678-9876');
+insert into empresa values('96239176000170', 'Extra', 'extra@administraçao.com.br', 'extrasuporte', '(11)7654-9812');
+insert into empresa values('09592804000100', 'Pão de Açucar', 'paodeaçucar@geral.com.br', 'paoacucar123', '(21)6578-3489');
+select * from empresa;
+delete from empresa where cnpj = '96239176000170';
 
 insert into usuario values(null, 'Marcelo Whitehead', '44039080805', '09592804000100');
 insert into usuario values(null, 'Milene Oliveira', '46037862803', '96239176000170');
 insert into usuario values(null, 'Yuri Vedovate', '45056764802', '92864872000116');
 insert into usuario values(null, 'Victor Samir', '65076865201', '09592804000100');
 select * from usuario;
-select supermercado.*, id_usuario, nome_user from supermercado, usuario where fkcnpj = cnpj;
+select empresa.*, id_usuario, nome_user from empresa, usuario where fkcnpj = cnpj;
 
 insert into sensores values(null, 'Hortifruti', '09592804000100');
 insert into sensores values(null, 'Bebidas', '09592804000100');
 insert into sensores values(null, 'Carnes', '96239176000170');
 insert into sensores values(null, 'Limpeza', '96239176000170');
-select supermercado.*, idsensor, area_sensor from supermercado, sensores where fkcnpj = cnpj;
+select empresa.*, idsensor, area_sensor from empresa, sensores where fkcnpj = cnpj;
 
 
 drop database eva;
